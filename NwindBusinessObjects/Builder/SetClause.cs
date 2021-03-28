96223292c5186c99298f4f9baf5ea6b8d117e7aa
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-03-28 22:15:25 +0300

    SetClause implement IDisposable

--- NwindBusinessObjects/Builder/SetClause.cs
+++ NwindBusinessObjects/Builder/SetClause.cs
@@ -8,7 +8,7 @@ using System.Data.SqlClient;
 using System.Reflection;
 
 namespace NwindBusinessObjects.Builder {
-    public class SetClause {
+    public class SetClause : IDisposable {
         private List<string> predicates;
         private List<SqlParameter> parameters;
 
@@ -68,5 +68,41 @@ namespace NwindBusinessObjects.Builder {
 
             return clause;
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing) {
+            if (!disposedValue) {
+                if (disposing) {
+                    // TODO: dispose managed state (managed objects).
+                    this.predicates.Clear();
+                    this.parameters.Clear();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+                this.predicates = null;
+                this.parameters = null;
+                this.skipColumns = null;
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~SetClause() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
