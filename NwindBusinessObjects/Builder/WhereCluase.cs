b6917d39f1e3317617a684b90466bca1e77d033f
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-03-28 22:20:44 +0300

    WhereCluase implement IDisposable

--- NwindBusinessObjects/Builder/WhereCluase.cs
+++ NwindBusinessObjects/Builder/WhereCluase.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 
 namespace NwindBusinessObjects.Builder {
-    public class WhereCluase {
+    public class WhereCluase : IDisposable {
         private List<Predicate> predicates;
         private List<SqlParameter> parameters;
 
@@ -162,9 +162,45 @@ namespace NwindBusinessObjects.Builder {
 
             return clause;
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing) {
+            if (!disposedValue) {
+                if (disposing) {
+                    // TODO: dispose managed state (managed objects).
+                    this.predicates.ForEach(predicate => predicate.Dispose());
+                    this.predicates.Clear();
+                    this.parameters.Clear();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+                this.predicates = null;
+                this.parameters = null;
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~WhereCluase() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 
-    abstract class Predicate {
+    abstract class Predicate : IDisposable {
         protected List<string> predicates;
 
         public Predicate() {
@@ -172,6 +208,39 @@ namespace NwindBusinessObjects.Builder {
         }
 
         public List<string> Predicates => this.predicates;
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing) {
+            if (!disposedValue) {
+                if (disposing) {
+                    // TODO: dispose managed state (managed objects).
+                    this.predicates.Clear();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+                this.predicates = null;
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Predicate() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose() {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 
     class AndPredicate : Predicate {
