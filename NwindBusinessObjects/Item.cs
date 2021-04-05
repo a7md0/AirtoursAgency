4465e306abc20518f4ff28632efbe4a90140149b
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-04-05 10:44:55 +0300

    Item add valid & errorMessage

--- NwindBusinessObjects/Item.cs
+++ NwindBusinessObjects/Item.cs
@@ -3,6 +3,9 @@ namespace NwindBusinessObjects {
     public abstract class Item {
         protected string id;
 
+        private bool valid;
+        private string errorMessage;
+
         public Item(string id) {
             this.id = id;
         }
@@ -10,6 +13,27 @@ namespace NwindBusinessObjects {
         public Item() { }
 
         public string Id => this.id;
+
+        public bool Valid {
+            get {
+                return this.valid;
+            }
+
+            set {
+                this.valid = value;
+            }
+        }
+
+        public string ErrorMessage {
+            get {
+                return this.errorMessage;
+            }
+
+            set {
+                this.errorMessage = value;
+            }
+        }
+
         public string getID() => this.id;
     }
 }
