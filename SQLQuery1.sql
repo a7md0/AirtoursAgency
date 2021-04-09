f8be3e0a18f359929f8749a516afdb79be2fc774
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-04-09 20:28:10 +0300

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿-- 7
+--SELECT DISTINCT [Manufacturer] FROM [Aircraft] WHERE [Manufacturer] IS NOT NULL ORDER BY [Manufacturer] ASC;
+
+-- 6
+--SELECT COUNT(*) FROM [ReservedSeat] WHERE [ScheduledFlightID] = 2 AND [Class] = 'Economy';
+
+-- 5
+SELECT COALESCE(SUM([Price]), 0) FROM [Reservation] WHERE [CustomerID] = 1 AND [Paid] = 0;
