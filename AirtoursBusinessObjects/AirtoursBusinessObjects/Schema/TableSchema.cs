fe4ea64ac58778716ba804a42b2309d6aad7577a
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-04-30 20:35:54 +0300

    TableSchema remove unused

--- AirtoursBusinessObjects/AirtoursBusinessObjects/Schema/TableSchema.cs
+++ AirtoursBusinessObjects/AirtoursBusinessObjects/Schema/TableSchema.cs
@@ -20,10 +20,6 @@ namespace AirtoursBusinessObjects.Schema {
                         value = null;
                     }
 
-                    if (value is null && property.PropertyType.IsValueType) {
-                        continue;
-                    }
-
                     property.SetValue(column, value);
                 }
 
@@ -37,35 +33,35 @@ namespace AirtoursBusinessObjects.Schema {
 
     public class TableSchemaColumn {
         public string ColumnName { get; set; }
-        public int ColumnOrdinal { get; set; }
-        public int ColumnSize { get; set; }
-        public short NumericPrecision { get; set; }
-        public short NumericScale { get; set; }
-        public bool IsUnique { get; set; }
-        public bool IsKey { get; set; }
-        public string BaseServerName { get; set; }
-        public string BaseCatalogName { get; set; }
-        public string BaseColumnName { get; set; }
-        public string BaseSchemaName { get; set; }
-        public string BaseTableName { get; set; }
-        public Type DataType { get; set; }
+        //public int ColumnOrdinal { get; set; }
+        //public int ColumnSize { get; set; }
+        //public short NumericPrecision { get; set; }
+        //public short NumericScale { get; set; }
+        //public bool IsUnique { get; set; }
+        //public bool IsKey { get; set; }
+        //public string BaseServerName { get; set; }
+        //public string BaseCatalogName { get; set; }
+        //public string BaseColumnName { get; set; }
+        //public string BaseSchemaName { get; set; }
+        //public string BaseTableName { get; set; }
+        //public Type DataType { get; set; }
         public bool AllowDBNull { get; set; }
-        public int ProviderType { get; set; }
-        public bool IsAliased { get; set; }
-        public bool IsExpression { get; set; }
-        public bool IsIdentity { get; set; }
+        //public int ProviderType { get; set; }
+        //public bool IsAliased { get; set; }
+        //public bool IsExpression { get; set; }
+        //public bool IsIdentity { get; set; }
         public bool IsAutoIncrement { get; set; }
-        public bool IsRowVersion { get; set; }
-        public bool IsHidden { get; set; }
-        public bool IsLong { get; set; }
-        public bool IsReadOnly { get; set; }
-        public Type ProviderSpecificDataType { get; set; }
-        public string DataTypeName { get; set; }
-        public string XmlSchemaCollectionDatabase { get; set; }
-        public string XmlSchemaCollectionOwningSchema { get; set; }
-        public string XmlSchemaCollectionName { get; set; }
-        public string UdtAssemblyQualifiedName { get; set; }
-        public int NonVersionedProviderType { get; set; }
-        public bool IsColumnSet { get; set; }
+        //public bool IsRowVersion { get; set; }
+        //public bool IsHidden { get; set; }
+        //public bool IsLong { get; set; }
+        //public bool IsReadOnly { get; set; }
+        //public Type ProviderSpecificDataType { get; set; }
+        //public string DataTypeName { get; set; }
+        //public string XmlSchemaCollectionDatabase { get; set; }
+        //public string XmlSchemaCollectionOwningSchema { get; set; }
+        //public string XmlSchemaCollectionName { get; set; }
+        //public string UdtAssemblyQualifiedName { get; set; }
+        //public int NonVersionedProviderType { get; set; }
+        //public bool IsColumnSet { get; set; }
     }
 }
