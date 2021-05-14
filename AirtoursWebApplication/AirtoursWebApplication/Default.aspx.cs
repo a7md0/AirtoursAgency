e0c856885979e6bc9c0c002610fd88d437be8257
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-14 04:40:58 +0300

    Default page print fName (tmp)

--- AirtoursWebApplication/AirtoursWebApplication/Default.aspx.cs
+++ AirtoursWebApplication/AirtoursWebApplication/Default.aspx.cs
@@ -5,10 +5,14 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using AirtoursBusinessObjects;
+
 namespace AirtoursWebApplication {
     public partial class _Default : Page {
         protected void Page_Load(object sender, EventArgs e) {
+            var cust = (AirtoursBusinessObjects.Customer) Session["customer"];
 
+            Label1.Text = cust?.Fname ?? "-";
         }
     }
 }
