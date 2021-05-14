a8fac6bebdde441bf683548338622d5b1d763c2a
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-14 04:27:47 +0300

    Create login page

---
+++ AirtoursWebApplication/AirtoursWebApplication/Login.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="AirtoursWebApplication.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h3>
+            <span>Login Page</span>
+        </h3>
+        <table>
+            <tr>
+                <td>Email:</td>
+                <td>
+                    <input id="txtUserName" type="text" runat="server" /></td>
+                <td>
+                    <asp:RequiredFieldValidator ControlToValidate="txtUserName"
+                        Display="Static" ErrorMessage="*" runat="server"
+                        ID="vUserName" /></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td>
+                    <input id="txtUserPass" type="password" runat="server" /></td>
+                <td>
+                    <asp:RequiredFieldValidator ControlToValidate="txtUserPass"
+                        Display="Static" ErrorMessage="*" runat="server"
+                        ID="vUserPass" />
+                </td>
+            </tr>
+        </table>
+        <asp:Button ID="cmdLogin" runat="server" Text="Login" OnClick="cmdLogin_Click" /><p></p>
+        <asp:Label ID="lblMsg" ForeColor="red" Font-Name="Verdana" Font-Size="10" runat="server" />
+    </form>
+</body>
+</html>
---
+++ AirtoursWebApplication/AirtoursWebApplication/Login.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+using AirtoursBusinessObjects;
+
+namespace AirtoursWebApplication {
+    public partial class Login : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void cmdLogin_Click(object sender, System.EventArgs e) {
+            var email = txtUserName.Value;
+            var password = txtUserPass.Value;
+
+            if ((null == email) || (0 == email.Length) || (email.Length > 40)) {
+                System.Diagnostics.Debug.WriteLine("[ValidateUser] Input validation of userName failed.");
+                return;
+            }
+
+            // Check for invalid passWord.
+            // passWord must not be null and must be between 1 and 25 characters.
+            if ((null == password) || (0 == password.Length) || (password.Length > 25)) {
+                System.Diagnostics.Debug.WriteLine("[ValidateUser] Input validation of passWord failed.");
+                return;
+            }
+
+            var customerList = new CustomerList();
+
+            var where = customerList.WhereClause
+                                    .Where("Email", email)
+                                    .Where("Password", password);
+
+            var customer = customerList.FindOne(where);
+
+            if (customer is null == false) {
+                Session.Add("customer", customer);
+
+                FormsAuthentication.RedirectFromLoginPage(customer.Email, true);
+            }
+        }
+    }
+}
