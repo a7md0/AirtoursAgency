64de3322b820531697b3c07a1d05cdfb60ea1a36
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-30 19:23:00 +0300

    Place new reservation implement

--- AirtoursWebApplication/AirtoursWebApplication/Reservations/New.aspx
+++ AirtoursWebApplication/AirtoursWebApplication/Reservations/New.aspx
@@ -1,5 +1,98 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="New.aspx.cs" Inherits="AirtoursWebApplication.Reservations.New" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="py-5 text-center">
+            <h2>Place reservation</h2>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4 order-md-2 mb-4">
+                <h4 class="d-flex justify-content-between align-items-center mb-3">
+                    <span class="text-muted">Your reservation</span>
+                </h4>
+                <ul class="list-group mb-3">
+                    <li class="list-group-item d-flex justify-content-between lh-condensed">
+                        <div>
+                            <h6 class="my-0">Outward flight</h6>
+                            <small class="text-muted">
+                                <asp:Label ID="OutwardFlightDescriptionLabel" runat="server"></asp:Label>
+                            </small>
+                        </div>
+                        <span class="text-muted">
+                            <asp:Label ID="OutwardFlightTotalLabel" runat="server" Text="$0"></asp:Label>
+                        </span>
+                    </li>
+                    <li class="list-group-item d-flex justify-content-between lh-condensed">
+                        <div>
+                            <h6 class="my-0">Return flight</h6>
+                            <small class="text-muted">
+                                <asp:Label ID="ReturnFlightDescriptionLabel" runat="server"></asp:Label>
+                            </small>
+                        </div>
+                        <span class="text-muted">
+                            <asp:Label ID="ReturnFlightTotalLabel" runat="server" Text="$0"></asp:Label>
+                        </span>
+                    </li>
+
+                    <li class="list-group-item d-flex justify-content-between">
+                        <span>Total (USD)</span>
+                        <strong>
+                            <asp:Label ID="FlightsTotalLabel" runat="server" Text="$0"></asp:Label>
+                        </strong>
+                    </li>
+                </ul>
+
+                <asp:Button ID="PlaceReservationButton" runat="server" class="btn btn-primary btn-lg btn-block" Text="Place Reservation" OnClick="PlaceReservationButton_Click" />
+            </div>
+
+            <div class="col-md-8 order-md-1">
+                <h4 class="mb-3">Reservation details</h4>
+
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <label for="state">Class</label>
+                        <asp:DropDownList ID="ClassDropDownList" runat="server" class="custom-select d-block w-100">
+                            <asp:ListItem Selected="True" Value="Economy">Economy class</asp:ListItem>
+                            <asp:ListItem Value="Business">Business class</asp:ListItem>
+                        </asp:DropDownList>
+                    </div>
+                </div>
+
+                <hr class="mb-4">
+
+                <h4 class="mb-3">Passengers</h4>
+                <div class="row">
+
+                    <div class="col-md-12 mb-3">
+                        <asp:GridView ID="PassengersGridView" runat="server" ShowHeaderWhenEmpty="true" DataKeyNames="PassengerID, ReservationID" CssClass="table table-bordered table-striped table-hover" AutoGenerateColumns="False">
+                            <Columns>
+                                <asp:BoundField HeaderText="PassengerID" DataField="PassengerID" ReadOnly="True" Visible="False" />
+                                <asp:BoundField DataField="FirstName" HeaderText="First name" ReadOnly="True" />
+                                <asp:BoundField DataField="LastName" HeaderText="Last name" ReadOnly="True" />
+                                <asp:BoundField HeaderText="ReservationID" DataField="ReservationID" ReadOnly="True" Visible="False" />
+                            </Columns>
+                        </asp:GridView>
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label for="FirstNameTextBox">First name:</label>
+                        <asp:TextBox ID="FirstNameTextBox" runat="server" class="form-control" minlength="2" maxlength="32" required="true"></asp:TextBox>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="LastNameTextBox">Last name</label>
+                        <asp:TextBox ID="LastNameTextBox" runat="server" class="form-control" minlength="2" maxlength="32" required="true"></asp:TextBox>
+                    </div>
+
+                    <div class="col-md-12 mb-3">
+                        <asp:Button ID="AddPassengerButton" runat="server" class="btn btn-primary btn-lg btn-block" Text="Add Passenger" OnClick="AddPassengerButton_Click" />
+                    </div>
+                </div>
+
+                <hr class="mb-4">
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- AirtoursWebApplication/AirtoursWebApplication/Reservations/New.aspx.cs
+++ AirtoursWebApplication/AirtoursWebApplication/Reservations/New.aspx.cs
@@ -5,10 +5,173 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using AirtoursBusinessObjects;
+
 namespace AirtoursWebApplication.Reservations {
     public partial class New : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+        protected static ReservationList reservationList = new ReservationList();
+        protected static PassengerList passengerList = new PassengerList();
+        protected static ReservedSeatList reservedSeatList = new ReservedSeatList();
 
+        protected Customer customer;
+
+        protected Flight outwardFlight;
+        protected ScheduledFlight outwardScheduledFlight;
+
+        protected Flight returnFlight;
+        protected ScheduledFlight returnScheduledFlight;
+
+        protected void Page_Load(object sender, EventArgs e) {
+            this.customer = (Customer) this.Session["customer"];
+
+            if (Session["Outward_Flight"] != null && Session["Outward_ScheduledFlight"] != null) {
+                this.outwardFlight = (Flight) Session["Outward_Flight"];
+                this.outwardScheduledFlight = (ScheduledFlight) Session["Outward_ScheduledFlight"];
+            }
+
+            if (Session["Return_Flight"] != null && Session["Return_ScheduledFlight"] != null) {
+                this.returnFlight = (Flight) Session["Return_Flight"];
+                this.returnScheduledFlight = (ScheduledFlight) Session["Return_ScheduledFlight"];
+            }
+
+            if (!Page.IsPostBack) {
+                this.Session["Passengers"] = new List<Passenger>();
+                this.AddPassenger(customer.Fname, customer.Lname);
+            }
+
+            this.PopulateControls();
+        }
+
+        private void PopulateControls() {
+
+        }
+
+        protected void AddPassengerButton_Click(object sender, EventArgs e) {
+            this.AddPassenger(this.FirstNameTextBox.Text, this.LastNameTextBox.Text);
+
+            this.FirstNameTextBox.Text = string.Empty;
+            this.LastNameTextBox.Text = string.Empty;
+        }
+
+        protected void PlaceReservationButton_Click(object sender, EventArgs e) {
+            List<Passenger> passengers = this.Session["Passengers"] as List<Passenger>;
+
+            decimal outwardFlightTotal = this.CalculateTotalFlight(this.outwardFlight);
+            decimal returnFlightTotal = this.CalculateTotalFlight(this.returnFlight);
+
+            List<int> passengerIDs = new List<int>();
+            bool failed = false;
+
+            // Create reservation
+            Reservation reservation = new Reservation() {
+                CustomerID = this.customer.CustomerID,
+                EmployeeID = null,
+                Paid = false,
+                ReservationDate = DateTime.Now,
+                Price = outwardFlightTotal + returnFlightTotal
+            };
+            reservationList.Add(reservation);
+            failed = !reservation.Valid;
+
+            if (reservation.Valid) {
+
+                // Create passengers (foreach)
+                foreach (Passenger passenger in passengers) {
+                    passenger.ReservationID = reservation.ReservationID;
+                    passengerList.Add(passenger);
+
+                    if (!passenger.Valid) {
+                        failed = true;
+                        break;
+                    }
+
+                    passengerIDs.Add(passenger.PassengerID);
+                }
+
+                // Create ReservedSeat (foreach)
+                foreach (int passengerID in passengerIDs) {
+                    ReservedSeat outwardReservedSeat = new ReservedSeat() {
+                        PassengerID = passengerID,
+                        ScheduledFlightID = this.outwardScheduledFlight.ScheduledFlightID,
+                        Class = this.ClassDropDownList.SelectedValue,
+                        Status = "Waitlisted",
+                        Sector = "Outward"
+                    };
+
+                    reservedSeatList.Add(outwardReservedSeat);
+
+                    if (!outwardReservedSeat.Valid) {
+                        failed = true;
+                        break;
+                    }
+
+                    if (this.returnFlight is null == false) {
+                        ReservedSeat returnReservedSeat = new ReservedSeat() {
+                            PassengerID = passengerID,
+                            ScheduledFlightID = this.returnScheduledFlight.ScheduledFlightID,
+                            Class = this.ClassDropDownList.SelectedValue,
+                            Status = "Waitlisted",
+                            Sector = "Return"
+                        };
+
+                        reservedSeatList.Add(returnReservedSeat);
+
+                        if (!returnReservedSeat.Valid) {
+                            failed = true;
+                            break;
+                        }
+                    }
+
+                }
+            }
+
+            if (!failed) {
+                Response.Redirect($"/Reservations/View.aspx?reservationID={reservation.ReservationID}", true);
+            } else {
+                Response.Write("Failed to place the reservation, please try again later.");
+            }
+        }
+
+        protected void AddPassenger(string firstName, string lastName) {
+            if (this.Session["Passengers"] == null) {
+                return;
+            }
+
+            List<Passenger> passengers = this.Session["Passengers"] as List<Passenger>;
+
+            Passenger passenger = new Passenger() {
+                FirstName = firstName,
+                LastName = lastName,
+            };
+
+            passengers.Add(passenger);
+        }
+
+        protected decimal CalculateTotalFlight(Flight flight) {
+            decimal total = 0m;
+            List<Passenger> passengers = this.Session["Passengers"] as List<Passenger>;
+
+            if (flight is null == false) {
+                total = flight.Fare ?? 0m;
+                total *= passengers.Count;
+            }
+
+            return total;
+        }
+
+        protected decimal CalculateTotalFlights() {
+            return this.CalculateTotalFlight(this.outwardFlight) + this.CalculateTotalFlight(this.returnFlight);
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e) {
+            List<Passenger> passengers = this.Session["Passengers"] as List<Passenger>;
+
+            this.PassengersGridView.DataSource = passengers;
+            this.PassengersGridView.DataBind();
+
+            this.OutwardFlightTotalLabel.Text = "$" + this.CalculateTotalFlight(this.outwardFlight).ToString("0.##");
+            this.ReturnFlightTotalLabel.Text = "$" + this.CalculateTotalFlight(this.returnFlight).ToString("0.##");
+            this.FlightsTotalLabel.Text = "$" + this.CalculateTotalFlights().ToString("0.##");
         }
     }
 }
