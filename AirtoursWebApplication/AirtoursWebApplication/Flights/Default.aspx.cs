6a38936c928b816116eb2072936e32c8da299e1d
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-30 23:05:10 +0300

    flight search remove unused

--- AirtoursWebApplication/AirtoursWebApplication/Flights/Default.aspx.cs
+++ AirtoursWebApplication/AirtoursWebApplication/Flights/Default.aspx.cs
@@ -15,26 +15,13 @@ namespace AirtoursWebApplication.Flights {
 
         protected void Page_Load(object sender, EventArgs e) {
             if (!Page.IsPostBack) {
-                List<string> origins = flightList.UniqueValues("Origin");
-                // List<string> destinations = flightList.UniqueValues("Destination");
                 this.Session["Outward_FlightIdx"] = null;
                 this.Session["Return_FlightIdx"] = null;
 
-                /*var whereClause = flightList.WhereClause;
-                whereClause.WhereIn("Origin", origins.ToArray());
-                List<string> destinations = flightList.UniqueValues("Destination", whereClause);*/
+                List<string> origins = flightList.UniqueValues("Origin");
 
                 this.OriginsDropDownList.DataSource = origins;
                 this.OriginsDropDownList.DataBind();
-
-                //this.DestinationsDropDownList.DataSource = destinations;
-                //this.DestinationsDropDownList.DataBind();
-
-                /*this.ReturnOriginDropDownList.DataSource = origins;
-                this.ReturnOriginDropDownList.DataBind();
-
-                this.ReturnDestinationDropDownList.DataSource = destinations;
-                this.ReturnDestinationDropDownList.DataBind();*/
 
                 if (this.DepartureDateTextBox.Text == "") {
                     this.DepartureDateTextBox.Text = DateTime.Now.ToString("yyyy-MM-dd");
@@ -56,12 +43,6 @@ namespace AirtoursWebApplication.Flights {
             bool showBookReturnFlight = this.BookReturnFlight.Checked;
 
             this.ReturnDateSection.Visible = showBookReturnFlight;
-
-
-            /*this.ReturnFlightControls.Visible = showBookReturnFlight;
-
-            this.ReturnOriginDropDownList.SelectedValue = this.DestinationsDropDownList.SelectedValue;
-            this.ReturnDestinationDropDownList.SelectedValue = this.OriginsDropDownList.SelectedValue;*/
         }
 
         protected void SearchButton_Click(object sender, EventArgs e) {
@@ -86,65 +67,6 @@ namespace AirtoursWebApplication.Flights {
             }
 
             Response.Redirect(route, endResponse: true);
-
-            /* if (this.DepartureDateTextBox.Text == string.Empty) {
-                return;
-            }
-
-            string origin = this.OriginsDropDownList.SelectedValue;
-            string destination = this.DestinationsDropDownList.SelectedValue;
-            DateTime departureDate = DateTime.ParseExact(this.DepartureDateTextBox.Text, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-
-            flightList.FlightsFilter(origin, destination, departureDate);
-
-            DataTable outwardDataTable = flightList.DataTable.Copy();
-
-           this.OutwardFlightsGridView.DataSource = outwardDataTable;
-            this.OutwardFlightsGridView.DataBind();
-
-            if (this.ReturnDateTextBox.Text != string.Empty) {
-                DateTime returnDate = DateTime.ParseExact(this.ReturnDateTextBox.Text, "yyyy-MM-dd", CultureInfo.InvariantCulture);
-
-                bool found = flightList.FlightsFilter(destination, origin, returnDate);
-
-                DataTable returnDataTable = flightList.DataTable.Copy();
-
-                this.ReturnFlightsGridView.DataSource = returnDataTable;
-                this.ReturnFlightsGridView.DataBind();
-            }*/
-        }
-
-        protected void ReserveFlightsButton_Click(object sender, EventArgs e) {
-            /*int? outwardFlightId = this.GetValueFromCheckedRow(this.OutwardFlightsGridView, 1);
-            int? returnFlightId = this.GetValueFromCheckedRow(this.ReturnFlightsGridView, 1);
-
-            if (!outwardFlightId.HasValue) {
-                Response.Write("Please select a flight");
-                return;
-            }
-
-            string redirectURL = $"/Reservations/New.aspx?outwardFlight={outwardFlightId.Value}";
-
-            if (returnFlightId.HasValue) {
-                redirectURL += $"&returnFlight={returnFlightId.Value}";
-            }
-
-            Response.Redirect(redirectURL);*/
-        }
-
-        protected int? GetValueFromCheckedRow(GridView gv, int cellIdx) {
-            int? flightID = null;
-
-            foreach (GridViewRow row in gv.Rows) {
-                var selectedCtrl = (CheckBox) row.FindControl("Selected");
-
-                if (selectedCtrl.Checked) {
-                    flightID = int.Parse(row.Cells[cellIdx].Text);
-                    break;
-                }
-            }
-
-            return flightID;
         }
     }
 }
