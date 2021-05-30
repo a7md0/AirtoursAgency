eb9965d8fe591f908753cb859e1e52729a605c7c
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-31 02:46:37 +0300

    Default reservation implement (future reservation)

--- AirtoursWebApplication/AirtoursWebApplication/Reservations/Default.aspx
+++ AirtoursWebApplication/AirtoursWebApplication/Reservations/Default.aspx
@@ -1,5 +1,24 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AirtoursWebApplication.Reservations.Default" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <asp:GridView ID="ReservationsGridView" runat="server" ShowHeaderWhenEmpty="True" DataKeyNames="ReservationID,CustomerID,EmployeeID" CssClass="table table-bordered table-striped table-hover" AutoGenerateColumns="False" OnSelectedIndexChanged="ReservationsGridView_SelectedIndexChanged">
+            <Columns>
+                <asp:CommandField ButtonType="Button" ShowSelectButton="True">
+                    <ControlStyle CssClass="btn btn-primary" />
+                </asp:CommandField>
+
+                <asp:BoundField HeaderText="ReservationID" DataField="ReservationID" ReadOnly="True" />
+                <asp:BoundField HeaderText="CustomerID" DataField="CustomerID" ReadOnly="True" Visible="False" />
+                <asp:BoundField HeaderText="EmployeeID" DataField="EmployeeID" ReadOnly="True" Visible="False" />
+
+                <asp:BoundField HeaderText="Paid" DataField="Paid" DataFormatString="{0:Yes;0;No}" />
+                <asp:BoundField HeaderText="Price" DataField="Price" DataFormatString="{0:C}" />
+                <asp:BoundField HeaderText="ReservationDate" DataField="ReservationDate" DataFormatString="{0:d}" />
+            </Columns>
+
+        </asp:GridView>
+    </div>
 </asp:Content>
--- AirtoursWebApplication/AirtoursWebApplication/Reservations/Default.aspx.cs
+++ AirtoursWebApplication/AirtoursWebApplication/Reservations/Default.aspx.cs
@@ -5,10 +5,38 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using AirtoursBusinessObjects;
+using AirtoursBusinessObjects.Builder;
+
 namespace AirtoursWebApplication.Reservations {
     public partial class Default : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+        protected Customer customer;
 
+        protected void Page_Load(object sender, EventArgs e) {
+            this.customer = (Customer) this.Session["customer"];
+
+            if (!Page.IsPostBack) {
+                ReservationList reservationList = new ReservationList();
+
+                var whereClause = reservationList.WhereClause;
+                whereClause.Where("CustomerID", this.customer.CustomerID);
+
+                var onClause = new WhereClause();
+                onClause.WhereDate("FlightDate", WhereOpreators.GreaterThan, DateTime.Parse("2015-12-31 00:00:00.000"));
+
+                reservationList.FilterJoin(whereClause, "Passenger", "ReservationID", "ReservedSeat", "PassengerID", "ScheduledFlight", "ScheduledFlightID", onClause);
+
+                this.ReservationsGridView.DataSource = reservationList.DataTable;
+                this.ReservationsGridView.DataBind();
+            }
+        }
+
+        protected void ReservationsGridView_SelectedIndexChanged(object sender, EventArgs e) {
+            GridView gridView = (GridView) sender;
+            GridViewRow selectedRow = gridView.SelectedRow;
+
+            object reservationID = gridView.DataKeys[selectedRow.RowIndex]["ReservationID"];
+            Response.Redirect($"/Reservations/View.aspx?reservationID={reservationID}", true);
         }
     }
 }
