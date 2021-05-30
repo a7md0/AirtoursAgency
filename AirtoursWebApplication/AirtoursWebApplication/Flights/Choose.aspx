c4c8b52499b6c5691f7662ebdaeb6cff3789c261
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-05-30 17:06:47 +0300

    Flights choose refactor

--- AirtoursWebApplication/AirtoursWebApplication/Flights/Choose.aspx
+++ AirtoursWebApplication/AirtoursWebApplication/Flights/Choose.aspx
@@ -9,20 +9,38 @@
                 <h3>Choose flight(s)</h3>
             </div>
         </div>
-        <asp:GridView ID="OutwardFlightsGridView" runat="server" CssClass="table table-bordered table-striped table-hover" Caption="Outward flights" CaptionAlign="Top" OnSelectedIndexChanged="OutwardFlightsGridView_SelectedIndexChanged">
+        <asp:GridView ID="OutwardFlightsGridView" runat="server" ShowHeaderWhenEmpty="true" DataKeyNames="FlightID" CssClass="table table-bordered table-striped table-hover" Caption="Outward flights" CaptionAlign="Top" OnSelectedIndexChanged="OutwardFlightsGridView_SelectedIndexChanged" AutoGenerateColumns="False">
             <Columns>
                 <asp:CommandField ButtonType="Button" ShowSelectButton="True">
                     <ControlStyle CssClass="btn btn-primary" />
                 </asp:CommandField>
+
+                <asp:BoundField HeaderText="FlightID" DataField="FlightID" ReadOnly="True" Visible="False" />
+                <asp:BoundField HeaderText="Flight Number" DataField="FlightNumber" ReadOnly="True" />
+                <asp:BoundField HeaderText="Origin" DataField="Origin" />
+                <asp:BoundField HeaderText="Destination" DataField="Destination" />
+                <asp:BoundField HeaderText="Departure Time" DataField="Departure" DataFormatString="{0:t}" />
+                <asp:BoundField HeaderText="Arrival Time" DataField="Arrival" DataFormatString="{0:t}" />
+                <asp:BoundField HeaderText="Airline" DataField="Airline" />
+                <asp:BoundField HeaderText="Fare" DataField="Fare" DataFormatString="{0:C}" />
             </Columns>
 
         </asp:GridView>
 
-        <asp:GridView ID="ReturnFlightsGridView" runat="server" CssClass="table table-bordered table-striped table-hover" Caption="Return flights" CaptionAlign="Top" OnSelectedIndexChanged="ReturnFlightsGridView_SelectedIndexChanged">
+        <asp:GridView ID="ReturnFlightsGridView" runat="server" ShowHeaderWhenEmpty="true" DataKeyNames="FlightID" CssClass="table table-bordered table-striped table-hover" Caption="Return flights" CaptionAlign="Top" OnSelectedIndexChanged="ReturnFlightsGridView_SelectedIndexChanged" AutoGenerateColumns="False">
             <Columns>
                 <asp:CommandField ButtonType="Button" ShowSelectButton="True">
                     <ControlStyle CssClass="btn btn-primary" />
                 </asp:CommandField>
+
+                <asp:BoundField HeaderText="FlightID" DataField="FlightID" ReadOnly="True" Visible="False" />
+                <asp:BoundField HeaderText="Flight Number" DataField="FlightNumber" ReadOnly="True" />
+                <asp:BoundField HeaderText="Origin" DataField="Origin" />
+                <asp:BoundField HeaderText="Destination" DataField="Destination" />
+                <asp:BoundField HeaderText="Departure Time" DataField="Departure" DataFormatString="{0:t}" />
+                <asp:BoundField HeaderText="Arrival Time" DataField="Arrival" DataFormatString="{0:t}" />
+                <asp:BoundField HeaderText="Airline" DataField="Airline" />
+                <asp:BoundField HeaderText="Fare" DataField="Fare" DataFormatString="{0:C}" />
             </Columns>
         </asp:GridView>
 
--- AirtoursWebApplication/AirtoursWebApplication/Flights/Choose.aspx.cs
+++ AirtoursWebApplication/AirtoursWebApplication/Flights/Choose.aspx.cs
@@ -15,39 +15,102 @@ namespace AirtoursWebApplication.Flights {
         protected string origin;
         protected string destination;
 
-        protected string departureDate;
-        protected string returnDate;
+        protected string departureString;
+        protected DateTime departureDate;
+
+        protected string returnString;
+        protected DateTime returnDate;
 
         protected static FlightList flightList = new FlightList();
+        protected static ScheduledFlightList scheduledFlightList = new ScheduledFlightList();
 
         protected void Page_Load(object sender, EventArgs e) {
             this.origin = this.Request.Params["origin"];
             this.destination = this.Request.Params["destination"];
 
-            this.departureDate = this.Request.Params["departureDate"];
-            this.returnDate = this.Request.Params["returnDate"];
+            this.departureString = this.Request.Params["departureDate"];
+            this.returnString = this.Request.Params["returnDate"];
 
             this.PopulateControl();
         }
 
+
+
         protected void PopulateControl() {
-            DateTime departureDate = DateTime.ParseExact(this.departureDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            this.departureDate = DateTime.ParseExact(this.departureString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
 
             flightList.FlightsFilter(origin, destination, departureDate);
 
-            this.OutwardFlightsGridView.DataSource = this.GenerateTableForFlight(flightList.List);
+            this.OutwardFlightsGridView.DataSource = flightList.DataTable.Copy();
             this.OutwardFlightsGridView.DataBind();
-            this.OutwardFlightsGridView.Caption = $"Outward flights ({this.departureDate})";
+            this.OutwardFlightsGridView.Caption = $"Outward flights ({this.departureString})";
 
-            if (this.returnDate != string.Empty && this.returnDate != null) {
-                DateTime returnDate = DateTime.ParseExact(this.returnDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            if (this.returnString != string.Empty && this.returnString != null) {
+                this.returnDate = DateTime.ParseExact(this.returnString, "yyyy-MM-dd", CultureInfo.InvariantCulture);
 
                 flightList.FlightsFilter(destination, origin, returnDate);
 
-                this.ReturnFlightsGridView.DataSource = this.GenerateTableForFlight(flightList.List);
+                this.ReturnFlightsGridView.DataSource = flightList.DataTable.Copy();
                 this.ReturnFlightsGridView.DataBind();
-                this.ReturnFlightsGridView.Caption = $"Return flights ({this.returnDate})";
+                this.ReturnFlightsGridView.Caption = $"Return flights ({this.returnString})";
             }
+        }
+
+        protected void OutwardFlightsGridView_SelectedIndexChanged(object sender, EventArgs e) {
+            GridView gridView = (GridView) sender;
+            GridViewRow selectedRow = gridView.SelectedRow;
+
+            object flightID = gridView.DataKeys[selectedRow.RowIndex]["FlightID"];
+            DateTime flightDate = this.departureDate;
+
+            ScheduledFlight scheduledFlight = this.FindScheduledFlight(flightID, flightDate);
+
+            if (scheduledFlight is null == false) { 
+                Session["Outward_FlightIdx"] = selectedRow.RowIndex;
+                Session["Outward_FlightID"] = flightID;
+                Session["Outward_ScheduledFlightID"] = scheduledFlight.ScheduledFlightID;
+                Session["Outward_FlightDate"] = scheduledFlight.FlightDate;
+            }
+        }
+
+        protected void ReturnFlightsGridView_SelectedIndexChanged(object sender, EventArgs e) {
+            GridView gridView = (GridView) sender;
+            GridViewRow selectedRow = gridView.SelectedRow;
+
+            object flightID = gridView.DataKeys[selectedRow.RowIndex]["FlightID"];
+            DateTime flightDate = this.returnDate;
+
+            ScheduledFlight scheduledFlight = this.FindScheduledFlight(flightID, flightDate);
+
+            if (scheduledFlight is null == false) {
+                Session["Return_FlightIdx"] = selectedRow.RowIndex;
+                Session["Return_FlightID"] = flightID;
+                Session["Return_ScheduledFlightID"] = scheduledFlight.ScheduledFlightID;
+                Session["Return_FlightDate"] = scheduledFlight.FlightDate;
+            }
+        }
+
+        protected void ReserveFlightsButton_Click(object sender, EventArgs e) {
+            object outwardFlightId = Session["Outward_FlightID"];
+            object returnFlightId = Session["Return_FlightID"];
+
+            if (outwardFlightId == null) {
+                Response.Write("Please select a flight");
+                return;
+            }
+
+            Response.Redirect("/Reservations/New.aspx");
+        }
+
+
+        protected ScheduledFlight FindScheduledFlight(object flightID, DateTime flightDate) {
+            AirtoursBusinessObjects.Builder.WhereClause whereClause = scheduledFlightList.WhereClause;
+            whereClause.Where("FlightID", flightID).WhereDate("FlightDate", flightDate);
+
+            return scheduledFlightList.FindOne(whereClause);
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e) {
 
             if (Session["Outward_FlightIdx"] != null) {
                 int idx = (int) Session["Outward_FlightIdx"];
@@ -58,99 +121,6 @@ namespace AirtoursWebApplication.Flights {
                 int idx = (int) Session["Return_FlightIdx"];
                 this.ReturnFlightsGridView.Rows[idx].BackColor = Color.Cyan;
             }
-        }
-
-        protected DataTable GenerateTableForFlight(List<Flight> flights) {
-            DataTable dataTable = new DataTable();
-
-            dataTable.Columns.Add(new DataColumn("Flight ID", typeof(int)));
-            dataTable.Columns.Add(new DataColumn("Flight Number", typeof(string)));
-            dataTable.Columns.Add(new DataColumn("Origin", typeof(string)));
-            dataTable.Columns.Add(new DataColumn("Destination", typeof(string)));
-            dataTable.Columns.Add(new DataColumn("Departure Time", typeof(string)));
-            dataTable.Columns.Add(new DataColumn("Arrival Time", typeof(string)));
-            dataTable.Columns.Add(new DataColumn("Airline", typeof(string)));
-            dataTable.Columns.Add(new DataColumn("Fare", typeof(string)));
-
-            foreach (Flight flight in flights) {
-                DataRow row = dataTable.NewRow();
-
-                row["Flight ID"] = flight.FlightID;
-                row["Flight Number"] = flight.FlightNumber;
-                row["Origin"] = flight.Origin;
-                row["Destination"] = flight.Destination;
-                row["Departure Time"] = flight.Departure?.ToShortTimeString() ?? "";
-                row["Arrival Time"] = flight.Arrival?.ToShortTimeString() ?? "";
-                row["Airline"] = flight.Airline;
-                row["Fare"] = $"${flight.Fare?.ToString("0.00")}";
-
-                dataTable.Rows.Add(row);
-            }
-
-            return dataTable;
-        }
-
-        protected void OutwardFlightsGridView_SelectedIndexChanged(object sender, EventArgs e) {
-            GridViewRow selectedRow = this.OutwardFlightsGridView.SelectedRow;
-
-            Session["Outward_FlightIdx"] = selectedRow.RowIndex;
-            Session["Outward_FlightID"] = int.Parse(selectedRow.Cells[1].Text);
-            /*Session["Outward_FlightNumber"] = selectedRow.Cells[2].Text;
-            Session["Outward_FlightAirline"] = selectedRow.Cells[7].Text;
-            Session["Outward_FlightOrigin"] = selectedRow.Cells[3].Text;
-            Session["Outward_FlightDestination"] = selectedRow.Cells[4].Text;
-            Session["Outward_FlightDate"] = null;
-            Session["Outward_FlightDepartureTime"] = selectedRow.Cells[5].Text;
-            Session["Outward_FlightArrivalTime"] = selectedRow.Cells[6].Text;*/
-
-            selectedRow.BackColor = Color.Cyan;
-        }
-
-        protected void ReturnFlightsGridView_SelectedIndexChanged(object sender, EventArgs e) {
-            GridViewRow selectedRow = this.ReturnFlightsGridView.SelectedRow;
-
-            Session["Return_FlightIdx"] = selectedRow.RowIndex;
-            Session["Return_FlightID"] = int.Parse(selectedRow.Cells[1].Text);
-            /*Session["Return_FlightNumber"] = selectedRow.Cells[7].Text;
-            Session["Return_FlightAirline"] = selectedRow.Cells[2].Text;
-            Session["Return_FlightOrigin"] = selectedRow.Cells[3].Text;
-            Session["Return_FlightDestination"] = selectedRow.Cells[4].Text;
-            Session["Return_FlightDate"] = null;
-            Session["Return_FlightDepartureTime"] = selectedRow.Cells[5].Text;
-            Session["Return_FlightArrivalTime"] = selectedRow.Cells[6].Text;*/
-
-            selectedRow.BackColor = Color.Cyan;
-        }
-
-        protected void ReserveFlightsButton_Click(object sender, EventArgs e) {
-            int? outwardFlightId = this.GetValueFromCheckedRow(this.OutwardFlightsGridView, "Outward", 1);
-            int? returnFlightId = this.GetValueFromCheckedRow(this.ReturnFlightsGridView, "Return", 1);
-
-            if (!outwardFlightId.HasValue) {
-                Response.Write("Please select a flight");
-                return;
-            }
-
-            string redirectURL = $"/Reservations/New.aspx?outwardFlight={outwardFlightId.Value}";
-
-            if (returnFlightId.HasValue) {
-                redirectURL += $"&returnFlight={returnFlightId.Value}";
-            }
-
-            Response.Redirect(redirectURL);
-        }
-
-        private int? GetValueFromCheckedRow(GridView gv, string sessionPrefix, int cellIdx) {
-
-            int? flightID = null;
-
-            if (Session[$"{sessionPrefix}_FlightIdx"] != null) {
-                int idx = (int) Session[$"{sessionPrefix}_FlightIdx"];
-
-                flightID = int.Parse(gv.Rows[idx].Cells[cellIdx].Text);
-            }
-
-            return flightID;
         }
     }
 }
