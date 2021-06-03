974b1c915eff9b2ada15648c0e934ff26613a92d
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-06-03 17:56:31 +0300

    [ABO] Add comments

--- AirtoursBusinessObjects/AirtoursBusinessObjects/ReservationList.cs
+++ AirtoursBusinessObjects/AirtoursBusinessObjects/ReservationList.cs
@@ -6,35 +6,49 @@ namespace AirtoursBusinessObjects {
     public class ReservationList : DataList<Reservation> {
         public ReservationList() : base() { }
 
-        public decimal ReservationsTotalPrice() {
+        /// <summary>
+        /// Query the total price for all reservations.
+        /// </summary>
+        /// <returns></returns>
+        public decimal TotalPrice() {
             return base.TotalValue<decimal>("Price");
         }
 
-        public decimal ReservationsTotalPrice(bool paid) {
-            WhereClause where = base.WhereClause.Where("Paid", paid);
+        /// <summary>
+        /// Query the total price for paid or unpaid reservations.
+        /// </summary>
+        /// <param name="paid"></param>
+        /// <returns></returns>
+        public decimal TotalPrice(bool paid) {
+            WhereClause where = base.WhereClause.Where("Paid", paid); // Construct where clause to match paid or unpaid
 
-            return base.TotalValue<decimal>("Price", where);
+            return base.TotalValue<decimal>("Price", where); // Call the base total value and pre-fill the column and the condition
         }
 
-        public decimal ReservationsTotalPrice(Customer customer) {
+        public decimal TotalPrice(Customer customer) {
             WhereClause where = base.WhereClause.Where("CustomerID", customer.GetId());
 
             return base.TotalValue<decimal>("Price", where);
         }
 
-        public decimal ReservationsTotalPrice(Customer customer, bool paid) {
+        public decimal TotalPrice(Customer customer, bool paid) {
             WhereClause where = base.WhereClause.Where("Paid", paid)
                                         .Where("CustomerID", customer.GetId());
 
             return base.TotalValue<decimal>("Price", where);
         }
 
-        public bool ReservationsFilter(Customer customer) {
+        public bool Filter(Customer customer) {
             WhereClause where = base.WhereClause.Where("CustomerID", customer.GetId());
 
             return base.Populate(where);
         }
 
+        /// <summary>
+        /// Filter only future reservations for given customer.
+        /// </summary>
+        /// <param name="customer">The customer to match</param>
+        /// <returns></returns>
         public bool FilterFutureReservations(Customer customer) {
             WhereClause whereClause = base.WhereClause;
             whereClause.Where("CustomerID", customer.CustomerID);
--- AirtoursBusinessObjects/AirtoursBusinessObjects/ReservedSeatList.cs
+++ AirtoursBusinessObjects/AirtoursBusinessObjects/ReservedSeatList.cs
@@ -4,11 +4,17 @@
     public class ReservedSeatList : DataListJoin<ReservedSeat> {
         public ReservedSeatList() : base() { }
 
+        /// <summary>
+        /// Query the total count of reserved seats in given scheduled flight.
+        /// </summary>
+        /// <param name="scheduledFlight">Scheduled flight to match</param>
+        /// <param name="class">Seat class (i.e. Economy or Business)</param>
+        /// <returns></returns>
         public int ReservedSeatsTotalCount(ScheduledFlight scheduledFlight, string @class) {
             WhereClause where = base.WhereClause.Where("ScheduledFlightID", scheduledFlight.ScheduledFlightID)
                             .Where("Class", @class);
 
-            return base.TotalCount(where);
+            return base.TotalCount(where); // Call the base total count, with the built where clause for conditions
         }
     }
 }
